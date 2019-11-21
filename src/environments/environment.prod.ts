3d7270aea424e6507b88edb6f7dcf7a0fd580e70
Adrian Ławecki <pancerro@gmail.com>
2019-11-21 22:46:26 +0100

    Heroku deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD1MtS_rC67UyJuAyagE2bLdTZdiWyF7PE",
+    authDomain: "mywebsite-c88a2.firebaseapp.com",
+    databaseURL: "https://mywebsite-c88a2.firebaseio.com",
+    projectId: "mywebsite-c88a2",
+    storageBucket: "mywebsite-c88a2.appspot.com",
+    messagingSenderId: "1041317221245",
+    appId: "1:1041317221245:web:00cb6aa184df5385c93214",
+    measurementId: "G-90QP6ZQEP9"
+  }
 };
