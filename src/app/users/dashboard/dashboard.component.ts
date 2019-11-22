b9dfe724f56af0e84a3ef6d4ba8c7291b2e4e8a6
Adrian Ławecki <pancerro@gmail.com>
2019-11-22 23:06:50 +0100

    hiroku

--- src/app/users/dashboard/dashboard.component.ts
+++ src/app/users/dashboard/dashboard.component.ts
@@ -1,34 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { CookieService } from "angular2-cookie/core";
-
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit{
+  ngOnInit(){
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+dragElement(document.getElementById("mydiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+  }
   user = this.authService.user;
   
   constructor(
     private router: Router,
-    private authService: AuthService,
-    private cookieService:CookieService
+    private authService: AuthService
   ) {}
-  ngOnInit(){
-    this.cookieService.put(this.authService.user.email, "test");
-    console.log("Set Test Cookie as Test");
-  }
-  getCookie(key: string) {
-    return this.cookieService.get(key);
+  myFunction() {
+    var para = document.createElement("P");
+    para.innerHTML = "To jest paragraf.";
+    document.getElementById("myDIV").appendChild(para);
   }
   logout() {
     this.authService.logout()
       .then(() => this.router.navigate(['/login']));
   }
-  dashboard(){
+  dashboardtwo(){
     this.router.navigate(['/dashboardtwo']);
   }
   
