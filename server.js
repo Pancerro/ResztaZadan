de0cf5c70fc8c025ccea1efd619be059122dfe8a
Adrian Ławecki <pancerro@gmail.com>
2019-11-22 22:24:43 +0100

    hiroku

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
