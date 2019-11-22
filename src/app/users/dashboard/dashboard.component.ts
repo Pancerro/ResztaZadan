517163400ce30b8f816f342e5f5a113a305b1a20
Adrian Ławecki <pancerro@gmail.com>
2019-11-23 00:00:44 +0100

    hiroku

--- src/app/users/dashboard/dashboard.component.ts
+++ src/app/users/dashboard/dashboard.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { CookieService } from "angular2-cookie/core";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit{
-  ngOnInit(){}
+  ngOnInit(){
+    this.cookieService.put(this.authService.user.email, "test");
+    console.log("Set Test Cookie as Test");
+  }
   user = this.authService.user;
   
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private cookieService:CookieService
   ) {}
-  myFunction() {
-    var para = document.createElement("P");
-    para.innerHTML = "To jest paragraf.";
-    document.getElementById("myDIV").appendChild(para);
+  getCookie(key: string) {
+    return this.cookieService.get(key);
   }
   logout() {
     this.authService.logout()
