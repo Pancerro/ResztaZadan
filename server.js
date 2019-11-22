c4da83ed0081b466ad100db0a64bc518a4a2bd2b
Adrian Ławecki <pancerro@gmail.com>
2019-11-22 05:12:57 +0100

    hiroku

--- server.js
+++ server.js
@@ -14,3 +14,4 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+app.use((req, res) => res.sendfile(__dirname + 'dashboard.component.html'));
