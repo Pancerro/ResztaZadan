78128f487c1f9048c1f8c424f84dd37ad42bdd0f
Adrian Ławecki <pancerro@gmail.com>
2019-11-28 01:59:13 +0100

    add footer

--- src/app/dashboard/welcome-site/welcome-site.component.ts
+++ src/app/dashboard/welcome-site/welcome-site.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,11 +6,9 @@ import { Router } from '@angular/router';
   templateUrl: './welcome-site.component.html',
   styleUrls: ['./welcome-site.component.css']
 })
-export class WelcomeSiteComponent implements OnInit {
+export class WelcomeSiteComponent {
 
   constructor(private router: Router) {}
-  ngOnInit() {
-  }
 login()
 {
   this.router.navigate(['/login']);
